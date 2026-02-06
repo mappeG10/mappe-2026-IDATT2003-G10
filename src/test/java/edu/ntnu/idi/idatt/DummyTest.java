d4288fb6b0b68073e0910c97a89e95d7b08d0a25
André Skjellevik <andskj@stud.ntnu.no>
2026-02-06 14:41:52 +0100

    add DummyTest class with a passing test and update pom.xml to change report phase to verify

---
+++ src/test/java/edu/ntnu/idi/idatt/DummyTest.java
@@ -0,0 +1,14 @@
+package edu.ntnu.idi.idatt;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+  @Test
+  void testDummyTestAlwaysPass() {
+    assertTrue(true);
+  }
+
+}
