4f48d334b7e4fb6f76d8750ecf1cc24dc2f28b07
André Skjellevik <andskj@stud.ntnu.no>
2026-02-12 14:01:03 +0100

    feat: implement PurchaseCalculator class for transaction calculations

---
+++ src/main/java/edu/ntnu/idi/idatt/models/PurchaseCalculator.java
@@ -0,0 +1,49 @@
+package edu.ntnu.idi.idatt.models;
+
+import java.math.BigDecimal;
+
+public class PurchaseCalculator implements TransactionCalculator {
+
+
+  private BigDecimal purchasePrice;
+  private BigDecimal quantity;
+
+
+  public PurchaseCalculator(Share share) {
+    this.purchasePrice = share.getPurchasePrice();
+    this.quantity = share.getQuantity();
+  }
+
+
+  /**
+   * @return
+   */
+  @Override
+  public BigDecimal calculateGross() {
+    return purchasePrice.multiply(quantity);
+  }
+
+  /**
+   * @return
+   */
+  @Override
+  public BigDecimal calculateCommission() {
+    return calculateGross().multiply(BigDecimal.valueOf(0.005));
+  }
+
+  /**
+   * @return
+   */
+  @Override
+  public BigDecimal calculateTax() {
+    return BigDecimal.ZERO;
+  }
+
+  /**
+   * @return
+   */
+  @Override
+  public BigDecimal calculateTotal() {
+    return calculateGross().subtract(calculateCommission()).subtract(calculateTax());
+  }
+}
