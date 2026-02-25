a6cc16b493a030badeeb9f53e4f2f414cc7a969a
Martin Aronsen <martaron@stud.ntnu.no>
2026-02-25 14:58:33 +0100

    Fixed implementation of the stock unit tests

--- src/test/java/edu/ntnu/idi/idatt/models/StockTest.java
+++ src/test/java/edu/ntnu/idi/idatt/models/StockTest.java
@@ -1,4 +1,73 @@
+package edu.ntnu.idi.idatt.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StockTest {
-  
+  Stock stock;
+  String symbol = "AAPL";
+  String company = "Apple";
+  List<BigDecimal> prices;
+
+  @BeforeEach
+  void setUp() {
+    prices = new ArrayList<>(List.of(
+        new BigDecimal("182.50"),
+        new BigDecimal("183.75"),
+        new BigDecimal("181.20")));
+
+    stock = new Stock(symbol, company, prices);
+  }
+
+  @Test
+  void testConstructorWithValidArguments() {
+    assertNotNull(stock, "Constructor should initialise correctly");
+    assertEquals(symbol, stock.getSymbol(),
+        "Constructor didn't initialise symbol correctly");
+    assertEquals(company, stock.getCompany(),
+        "Constructor didn't initialise company name correctly");
+    assertEquals(prices.getLast(), stock.getSalesPrice(),
+        "Constructor didn't initialise sales price correctly");
+  }
+  @Test
+  void testConstructorWithInvalidArguments() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      new Stock("", company, prices);
+    }, "Constructor should throw IllegalArgumentException");
+    assertThrows(IllegalArgumentException.class, () -> {
+      new Stock(symbol, "", prices);
+    }, "Constructor should throw IllegalArgumentException");
+  }
+
+  @Test
+  void testConstructorWithNullArguments() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      new Stock(null, company, prices);
+    }, "Constructor should throw IllegalArgumentException");
+    assertThrows(IllegalArgumentException.class, () -> {
+      new Stock(symbol, null, prices);
+    }, "Constructor should throw IllegalArgumentException");
+    assertThrows(IllegalArgumentException.class, () -> {
+      new Stock(symbol, company, null);
+    },  "Constructor should throw IllegalArgumentException");
+  }
+
+  @Test
+  void testAddNewSalesPrice() {
+    BigDecimal newPrice = new BigDecimal("183.50");
+    stock.addNewSalesPrice(newPrice);
+    assertEquals(newPrice, stock.getSalesPrice(), "New sales price was not added correctly");
+  }
+  @Test
+  void testAddNewSalesPriceThrowsException() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      stock.addNewSalesPrice(null);
+    }, "addNewSalesPrice should throw IllegalArgumentException");
+  }
 }
