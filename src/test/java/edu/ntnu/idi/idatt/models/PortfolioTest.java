1f8d79f2076e12e3aea8101eb9b984587929c662
Martin <63729025+martiaro05@users.noreply.github.com>
2026-02-25 19:38:53 +0100

    Feat/unit test portfolio stock share (#17)
    * Implemented unit tests for the stock model
    
    * Implemented unit tests for the share model
    
    * Implemented unit tests for the portfolio model
    
    * Fixed implementation of the stock unit tests
    
    * Rewriting portfolio unit tests due to lack of clarity in tests
    
    * Added full test coverage to the portfolio class

---
+++ src/test/java/edu/ntnu/idi/idatt/models/PortfolioTest.java
@@ -0,0 +1,86 @@
+package edu.ntnu.idi.idatt.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PortfolioTest {
+
+  Portfolio portfolio;
+
+  Stock stock1 = new Stock("APPL", "Apple", new ArrayList<>());
+  Stock stock2 = new Stock("GOOG", "Alphabet ", new ArrayList<>());
+
+  Share share1 = new Share(stock1, new BigDecimal(1), new BigDecimal("182.5"));
+  Share share2 = new Share(stock2, new BigDecimal(1), new BigDecimal("310.2"));
+
+  @BeforeEach
+  void setUp() {
+    portfolio = new Portfolio();
+    portfolio.addShare(share1);
+    portfolio.addShare(share2);
+  }
+
+  @Test
+  void testPortfolioConstructor() {
+    assertNotNull(portfolio, "Portfolio should not be null when constructed.");
+  }
+
+  @Test
+  void testAddShare() {
+    assertTrue(portfolio.addShare(share1), "Share 1 should be added.");
+    assertTrue(portfolio.addShare(share2), "Share 2 should be added.");
+  }
+
+  @Test
+  void testAddShareFails() {
+    assertFalse(portfolio.addShare(null), "Null should not be added as a share");
+  }
+
+  @Test
+  void testGetShares() {
+    assertEquals(2, portfolio.getShares().size());
+    assertEquals(share1, portfolio.getShares().getFirst(), "Shares should match");
+    assertEquals(share2, portfolio.getShares().getLast(), "Shares should match.");
+  }
+
+  @Test
+  void testGetSharesWithStockSymbol() {
+    assertEquals(1, portfolio.getShares("GOOG").size(),
+        "List size should be 1");
+    assertEquals(share1, portfolio.getShares("APPL").getFirst(),
+        "Shares should match");
+    assertTrue(portfolio.getShares("META").isEmpty(),
+        "Shares should be empty");
+  }
+  @Test
+  void testContainsShare() {
+    Share share3 = new Share(stock1, new BigDecimal(10), new BigDecimal(1865));
+
+    assertTrue(portfolio.contatins(share1),
+        "Portfolio should contain share 1.");
+    assertTrue(portfolio.contatins(share2),
+        "Portfolio should contain share 2.");
+    assertFalse(portfolio.contatins(share3),
+        "Portfolio should not contain share 3.");
+  }
+
+  @Test
+  void testRemoveShare() {
+    Share share3 = new Share(stock1, new BigDecimal(10), new BigDecimal(1865));
+
+    assertTrue(portfolio.removeShare(share1),
+        "Share 1 should be removed successfully.");
+    assertTrue(portfolio.removeShare(share2),
+        "Share 2 should be removed successfully.");
+    assertFalse(portfolio.removeShare(share3),
+        "Share 3 should not exist in portfolio");
+  }
+
+
+
+}
