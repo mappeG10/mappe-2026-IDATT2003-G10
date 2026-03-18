eb8f5e5912add1fe84e6b9f0aeec04c80d1746d8
Martin Aronsen <martaron@stud.ntnu.no>
2026-03-18 11:25:35 +0100

    Refactored code to remove SonarQube nested warning

--- src/main/java/edu/ntnu/idi/idatt/dal/StockParser.java
+++ src/main/java/edu/ntnu/idi/idatt/dal/StockParser.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class StockParser {
 
@@ -17,25 +18,41 @@ public class StockParser {
 
     try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
       while ((line = br.readLine()) != null) {
-        if (line.startsWith("#") || line.trim().isBlank()) {
+        if (isSkippable(line)) {
           continue;
         }
-
-        String[] data = line.split(",");
-
-        String symbol = data[0].trim();
-        String name = data[1].trim();
-        BigDecimal price = new BigDecimal(data[2].trim());
-
-        try {
-          stocks.add(new Stock(symbol, name, new ArrayList<>(List.of(price))));
-        } catch (IllegalArgumentException e) {
-          e.printStackTrace();
-        }
+        parseLineToStock(line).ifPresent(stocks::add);
       }
       return stocks;
     } catch (IOException e) {
       throw new RuntimeException(e); // TODO: use custom exceptions later
     }
   }
+
+  private static boolean isSkippable(String line) {
+    return line.startsWith("#") || line.trim().isBlank();
+  }
+
+  private static Optional<Stock> parseLineToStock(String line) {
+    try {
+      String[] data = line.split(",");
+
+      if (data.length < 3) {
+        return Optional.empty();
+      }
+
+      String symbol = data[0].trim();
+      String name = data[1].trim();
+      BigDecimal price = new BigDecimal(data[2].trim());
+
+      return Optional.of(
+          new Stock(symbol, name, new ArrayList<>(List.of(price))));
+
+    } catch (IllegalArgumentException e) {
+      System.err.println(e);
+      System.err.println("Skipping malformed stock row: " + line);
+      return Optional.empty();
+    }
+  }
+
 }
