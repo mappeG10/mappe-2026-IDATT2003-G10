7b192813568de6fdd13990167bd9f11b80a6d9db
André Skjellevik <andskj@stud.ntnu.no>
2026-03-24 00:10:41 +0100

    add tests for TransactionArchive to verify empty state and distinct week counting

--- src/test/java/edu/ntnu/idi/idatt/models/TransactionArchiveTest.java
+++ src/test/java/edu/ntnu/idi/idatt/models/TransactionArchiveTest.java
@@ -1,6 +1,7 @@
 package edu.ntnu.idi.idatt.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
@@ -44,7 +45,7 @@ public class TransactionArchiveTest {
 
   @Test
   void testTransactionArchiveConstructor() {
-
+    assertTrue(transactionArchive.isEmpty(), "Should be empty on creation");
   }
 
 
@@ -119,11 +120,36 @@ public class TransactionArchiveTest {
   @Test
   void testCountDistinctWeeks() {
 
+    Purchase firstWeekFirstPurchase = new Purchase(share, 1);
+    Purchase secondWeekFirstPurchase = new Purchase(share, 2);
+    Purchase thirdWeekFirstPurchase = new Purchase(share, 3);
+    Purchase thirdWeekSecondPurchase = new Purchase(share, 3);
 
 
-    player.getTransactionArchive().countDistinctWeeks();
+    firstWeekFirstPurchase.commit(player);
+    secondWeekFirstPurchase.commit(player);
+    thirdWeekFirstPurchase.commit(player);
+    thirdWeekSecondPurchase.commit(player);
+
+
+    assertEquals(3, transactionArchive.countDistinctWeeks(),
+        "Should be 3 distinct weeks in the transaction archive");
 
   }
 
+
+  @Test
+  void testIsEmpty() {
+    assertTrue(transactionArchive.isEmpty(), "Transaction archive should be empty when constructed");
+  }
+
+  @Test
+  void testIsNotEmpty() {
+    Purchase purchase = new Purchase(share, 1);
+
+    purchase.commit(player);
+
+    assertFalse(transactionArchive.isEmpty(), "Transaction archive should not be empty after a commit");
+  }
 
 }
