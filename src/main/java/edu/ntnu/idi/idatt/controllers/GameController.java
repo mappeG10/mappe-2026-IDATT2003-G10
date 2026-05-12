9bf8db66cca8e88a12e5fb219796e22e1e2a20a6
André Skjellevik <42360069+askjellevik@users.noreply.github.com>
2026-05-12 15:11:51 +0200

    add GameController class to manage main game state and delegate sub-controllers

---
+++ src/main/java/edu/ntnu/idi/idatt/controllers/GameController.java
@@ -0,0 +1,65 @@
+package edu.ntnu.idi.idatt.controllers;
+
+import edu.ntnu.idi.idatt.models.Exchange;
+import edu.ntnu.idi.idatt.models.Player;
+import edu.ntnu.idi.idatt.view.GameObserver;
+import java.math.BigDecimal;
+
+public class GameController {
+
+  private final Exchange exchange;
+  private final Player player;
+  private final MarketController marketController;
+  private final TransactionController transactionController;
+  private final PortfolioController portfolioController;
+
+  public GameController(Exchange exchange, Player player) {
+    this.exchange = exchange;
+    this.player = player;
+    this.marketController =  new MarketController(exchange, player);
+    this.transactionController = new TransactionController(exchange, player);
+    this.portfolioController = new PortfolioController(exchange, player);
+  }
+
+  public void registerObserver(GameObserver observer) {
+    exchange.register(observer);
+    player.register(observer);
+  }
+
+  public void unregisterObserver(GameObserver observer) {
+    exchange.unregister(observer);
+    player.unregister(observer);
+  }
+
+  public MarketController getMarketController() {
+    return marketController;
+  }
+
+  public TransactionController getTransactionController() {
+    return transactionController;
+  }
+
+  public PortfolioController getPortfolioController() {
+    return portfolioController;
+  }
+
+  public int getCurrentWeek() {
+    return exchange.getWeek();
+  }
+
+  public String getPlayerName() {
+    return player.getName();
+  }
+
+  public BigDecimal getPlayerMoney() {
+    return player.getMoney();
+  }
+
+  public BigDecimal getNetWorth() {
+    return player.getNetWorth();
+  }
+
+  public void advanceWeek() {
+    exchange.advance();
+  }
+}
