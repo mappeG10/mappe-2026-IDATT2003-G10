e1b053ba0130bacc2b8e9abaede6a17b49ab4065
Martin Aronsen <martaron@stud.ntnu.no>
2026-05-13 13:53:44 +0200

    Changed Main class to App for better name clearity

---
+++ src/main/java/edu/ntnu/idi/idatt/App.java
@@ -0,0 +1,17 @@
+package edu.ntnu.idi.idatt;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+
+
+  @Override
+  public void start(Stage stage) throws Exception {
+
+  }
+  static void main() {
+
+  }
+}
