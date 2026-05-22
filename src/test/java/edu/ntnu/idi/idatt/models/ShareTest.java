90b319d3f34cd00c8ae01c23fff35f4928a3326e
André Skjellevik <42360069+askjellevik@users.noreply.github.com>
2026-05-22 15:51:57 +0200

    add gain/loss calculation methods and corresponding tests

--- src/test/java/edu/ntnu/idi/idatt/models/ShareTest.java
+++ src/test/java/edu/ntnu/idi/idatt/models/ShareTest.java
@@ -64,4 +64,73 @@ class ShareTest {
 
     assertEquals(0, excpectedValue.compareTo(share.getCurrentValue()));
   }
+
+  @Test
+  void testGetGainLoss() {
+    List<BigDecimal> gainPrices = new ArrayList<>(List.of(
+        new BigDecimal("100"),
+        new BigDecimal("150")));
+    Stock gainStock = new Stock("MSFT", "Microsoft", gainPrices);
+    Share gainShare = new Share(gainStock, new BigDecimal("10"), new BigDecimal("100"));
+
+    BigDecimal expectedGainLoss = new BigDecimal("500");
+
+    assertEquals(0, expectedGainLoss.compareTo(gainShare.getGainLoss()),
+        "Gain/loss should be 500 when 10 shares bought at 100 are now worth 150");
+  }
+
+  @Test
+  void testGetGainLossNegative() {
+    List<BigDecimal> lossPrices = new ArrayList<>(List.of(
+        new BigDecimal("100"),
+        new BigDecimal("50")));
+    Stock lossStock = new Stock("MSFT", "Microsoft", lossPrices);
+    Share lossShare = new Share(lossStock, new BigDecimal("10"), new BigDecimal("100"));
+
+    BigDecimal expectedGainLoss = new BigDecimal("-500");
+
+    assertEquals(0, expectedGainLoss.compareTo(lossShare.getGainLoss()),
+        "Gain/loss should be -500 when 10 shares bought at 100 are now worth 50");
+  }
+
+  @Test
+  void testGetGainLossPercent() {
+    List<BigDecimal> gainPrices = new ArrayList<>(List.of(
+        new BigDecimal("100"),
+        new BigDecimal("150")));
+    Stock gainStock = new Stock("MSFT", "Microsoft", gainPrices);
+    Share gainShare = new Share(gainStock, new BigDecimal("10"), new BigDecimal("100"));
+
+    BigDecimal expectedPercent = new BigDecimal("50");
+
+    assertEquals(0, expectedPercent.compareTo(gainShare.getGainLossPercent()),
+        "Gain/loss percent should be 50% when current price is 50% higher than purchase price");
+  }
+
+  @Test
+  void testGetGainLossPercentNegative() {
+    List<BigDecimal> lossPrices = new ArrayList<>(List.of(
+        new BigDecimal("100"),
+        new BigDecimal("50")));
+    Stock lossStock = new Stock("MSFT", "Microsoft", lossPrices);
+    Share lossShare = new Share(lossStock, new BigDecimal("10"), new BigDecimal("100"));
+
+    BigDecimal expectedPercent = new BigDecimal("-50");
+
+    assertEquals(0, expectedPercent.compareTo(lossShare.getGainLossPercent()),
+        "Gain/loss percent should be -50% when current price is 50% lower than purchase price");
+  }
+
+  @Test
+  void testGetGainLossPercentWithZeroPurchasePrice() {
+    List<BigDecimal> prices = new ArrayList<>(List.of(new BigDecimal("100")));
+    Stock zeroPriceStock = new Stock("MSFT", "Microsoft", prices);
+    Share zeroPriceShare = new Share(zeroPriceStock, new BigDecimal("10"), BigDecimal.ZERO);
+
+    BigDecimal expectedPercent = BigDecimal.ZERO;
+
+    assertEquals(0, expectedPercent.compareTo(zeroPriceShare.getGainLossPercent()),
+        "Gain/loss percent should be 0% when purchase price is zero");
+  }
+
 }
