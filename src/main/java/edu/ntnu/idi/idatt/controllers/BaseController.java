5a8b19592f3b8bd65d3f36af52469575b317aeef
Martin Aronsen <martaron@stud.ntnu.no>
2026-05-24 19:55:19 +0200

    Implemented an abstract base controller

---
+++ src/main/java/edu/ntnu/idi/idatt/controllers/BaseController.java
@@ -0,0 +1,26 @@
+package edu.ntnu.idi.idatt.controllers;
+
+import edu.ntnu.idi.idatt.models.Exchange;
+import edu.ntnu.idi.idatt.models.Player;
+import edu.ntnu.idi.idatt.observer.GameObserver;
+
+public abstract class BaseController {
+  protected final Exchange exchange;
+  protected final Player player;
+
+  protected BaseController(Exchange exchange, Player player) {
+    this.exchange = exchange;
+    this.player = player;
+  }
+
+  protected void registerObserver(GameObserver observer) {
+    exchange.register(observer);
+    player.register(observer);
+  }
+
+  protected void unregisterObserver(GameObserver observer) {
+    exchange.unregister(observer);
+    player.unregister(observer);
+  }
+
+}
